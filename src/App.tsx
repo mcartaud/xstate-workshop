c0adbd7038ff2c54d4f14a5300fbf648488d53b9
Mathieu Cartaud <dev@mcartaud.com>
2025-01-26 19:37:01 +0100

    Add initial components

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+function padTime(minsOrSecs: number) {
+  return minsOrSecs < 10 ? `0${minsOrSecs}` : minsOrSecs;
 }
 
-export default App
+function App() {
+  return (
+    <div className="App">
+      <h1>
+        {padTime(0)}:{padTime(0)}
+      </h1>
+      <button>min</button>
+      <button>sec</button>
+      <button>reset</button>
+      <button>start</button>
+      <button>pause</button>
+    </div>
+  );
+}
+
+export default App;
